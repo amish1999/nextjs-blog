10b0d05578ed1ff9dcb0608b2527f8ecf31bdb4a
amish1999 <47895005+amish1999@users.noreply.github.com>
2020-05-08 01:06:30 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# nextjs-blog
-A NextJs blog 
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/api/hello.js
@@ -0,0 +1,9 @@
+export default (req, res) => {
+   res.status(200).json({ text: 'Hello' })
+  }
+ 
+
+  //export default (req, res) => {
+    //const email = req.body.email
+    // Then save email to your database, etc...
+  //}  
